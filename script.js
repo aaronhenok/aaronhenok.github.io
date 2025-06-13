644b324e619a4d15dd085e488f91ba552dc7047c
aaronhenok <77549621+aaronhenok@users.noreply.github.com>
2025-06-12 23:25:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// script.js
 function changeColor() {
   const body = document.body;
   const heading = document.getElementById('heading');
 
-  // Toggle background color
-  body.style.backgroundColor = body.style.backgroundColor === 'lightblue' ? 'white' : 'lightblue';
-
-  // Change the heading text
+  body.classList.toggle('colored');
   heading.textContent = "You clicked the button!";
 }
